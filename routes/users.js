eb4e5278dabddf6633378340e431ff1b06c8bf49
Zach Buchenau <ZBuchenau@yahoo.com>
2016-05-04 12:08:20 -0600

    connected the front and backend so they can communicate through http

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https');
+var jwt = require('jsonwebtoken');
+var knex = require('../db/knex.js');
 
-/* GET users listing. */
+//====================================================
+//  INDEX
+//====================================================
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+
+
+//====================================================
+//  SIGNUP (Enter User into the Database)
+//====================================================
+router.post('/signup', function(req, res, next){
+  console.log('user has hit the /USERS/SIGNUP endpoint');
+  console.log(req.body);
+});
+
+
+
+//====================================================
+//  LOGIN (Authenticate User with the Database)
+//====================================================
+router.post('/login', function(req, res, next){
+  console.log('user has hit the /USERS/LOGIN endpoint');
+  console.log(req.body);
 });
 
 module.exports = router;
