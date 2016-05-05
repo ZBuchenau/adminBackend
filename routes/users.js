782181c4408f4ddd9a59a30c28b60d4899e28868
Zach Buchenau <ZBuchenau@yahoo.com>
2016-05-05 12:00:36 -0600

    added bcrypt comparison, and JWT generation

--- routes/users.js
+++ routes/users.js
@@ -9,7 +9,7 @@ var bcrypt = require('bcrypt');
 //  INDEX
 //====================================================
 router.get('/', function(req, res, next) {
-  res.send('Waiting for User requests');
+    console.log(req.body);
 });
 
 
@@ -17,62 +17,62 @@ router.get('/', function(req, res, next) {
 //====================================================
 //  SIGNUP (Enter User into the Database)
 //====================================================
-router.post('/signup', function(req, res, next){
-  console.log('user has hit the /USERS/SIGNUP endpoint');
-  console.log(req.body);
+router.post('/signup', function(req, res, next) {
+    console.log('user has hit the /USERS/SIGNUP endpoint');
+    console.log(req.body);
 
 
 
-  var userFirstName = req.body.firstName;
-  var userLastName = req.body.lastName;
-  var username = req.body.userName;
-  var userEmail = req.body.email;
-  var userPassword = req.body.password;
+    var userFirstName = req.body.firstName;
+    var userLastName = req.body.lastName;
+    var username = req.body.userName;
+    var userEmail = req.body.email;
+    var userPassword = req.body.password;
 
-  var saltRounds = 10;
-  var salt = bcrypt.genSaltSync(saltRounds);
-  var hash = bcrypt.hashSync(userPassword, salt);
+    var saltRounds = 13;
+    var salt = bcrypt.genSaltSync(saltRounds);
+    var hash = bcrypt.hashSync(userPassword, salt);
 
-  knex('users')
-    .where('email', userEmail)
-    .then(function(response){
-      console.log(response);
+    knex('users')
+        .where('email', userEmail)
+        .then(function(response) {
+            console.log(response);
 
-      if(response.length === 0){
-        knex('users')
-          .returning('id')
-          .insert({
-            'first_name' : userFirstName,
-            'last_name' : userLastName,
-            'username' : username,
-            'password' : hash,
-            'email' : userEmail
-          })
-          .then(function(response){
-            console.log('User ' + response + ' Has Been Placed In Database' );
-            var myToken = jwt.sign({
-              id : response
-            }, process.env.JWT_SECRET);
+            if (response.length === 0) {
+                knex('users')
+                    .returning('id')
+                    .insert({
+                        'first_name': userFirstName,
+                        'last_name': userLastName,
+                        'username': username,
+                        'password': hash,
+                        'email': userEmail
+                    })
+                    .then(function(response) {
+                        console.log('User ' + response + ' Has Been Placed In Database');
+                        var myToken = jwt.sign({
+                            id: response
+                        }, process.env.JWT_SECRET);
 
-            console.log(myToken);
+                        console.log(myToken);
 
-            res.send({
-              token : myToken
-            });
-          })
-          .catch(function(err){
+                        res.send({
+                            token: myToken
+                        });
+                    })
+                    .catch(function(err) {
+                        console.log(err);
+                    });
+
+            } else {
+                console.log('User already exists in the database');
+                res.end();
+            }
+
+        })
+        .catch(function(err) {
             console.log(err);
-          });
-
-      } else {
-        console.log('User already exists in the database');
-        res.end();
-      }
-
-    })
-    .catch(function(err){
-      console.log(err);
-    });
+        });
 });
 
 
@@ -80,9 +80,55 @@ router.post('/signup', function(req, res, next){
 //====================================================
 //  LOGIN (Authenticate User with the Database)
 //====================================================
-router.post('/login', function(req, res, next){
-  console.log('user has hit the /USERS/LOGIN endpoint');
-  console.log(req.body);
+router.post('/login', function(req, res, next) {
+    console.log('user has hit the /USERS/LOGIN endpoint');
+    console.log(req.body);
+
+    var userEmail = req.body.email;
+    var userPassword = req.body.password;
+
+    knex('users')
+      .where({
+        'email' : userEmail,
+      })
+      .then(function(response){
+        var userInfo = response[0];
+
+        if(bcrypt.compareSync(userPassword, userInfo.password)){
+
+          userInfo = {
+            id: userInfo.id,
+            firstname: userInfo.first_name,
+            lastname: userInfo.last_name,
+            username: userInfo.username
+          };
+
+          return userInfo;
+
+        } else {
+          res.send(401, 'Wrong Email or Password');
+        }
+      })
+      .then(function(response){
+
+        var myToken = jwt.sign({
+            id: response.id
+        }, process.env.JWT_SECRET);
+
+        var user = response;
+        user.token = myToken;
+
+        console.log('all good up until here');
+
+        res.send(user);
+
+      })
+      .catch(function(err){
+        console.log(err + 'ERROR');
+      });
+
+
+
 });
 
 module.exports = router;
