851b5b08f1ab9ee0746d4d329f588a6e3538f962
Zach Buchenau <ZBuchenau@yahoo.com>
2016-09-20 15:54:41 -0600

    removed the ssl key again

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      database: process.env.DATABASE_URL + '/ssl=true'
+      database: process.env.DATABASE_URL
     },
     pool: {
       min: 2,
