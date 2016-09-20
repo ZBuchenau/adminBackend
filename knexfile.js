f8a19e38b1ff91722052969ffe524fb0c8abfd21
Zach Buchenau <ZBuchenau@yahoo.com>
2016-09-20 15:52:50 -0600

    added the database back in there

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,9 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL + '/ssl=true',
+    connection: {
+      database: process.env.DATABASE_URL + '/ssl=true'
+    },
     pool: {
       min: 2,
       max: 10
