86b42137069aacf0aa9a92214d0c5b46b1a985af
Zach Buchenau <ZBuchenau@yahoo.com>
2016-09-20 15:56:27 -0600

    still working

--- knexfile.js
+++ knexfile.js
@@ -14,9 +14,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: process.env.DATABASE_URL
-    },
+    connection: process.env.DATABASE_URL + '/ssl=true',
     pool: {
       min: 2,
       max: 10
