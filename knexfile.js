26051de75867ffcc5f0942c80fe5fd1f1b1b96aa
Zach Buchenau <ZBuchenau@yahoo.com>
2016-09-20 16:02:33 -0600

    still

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: "postgres://sqinpifqapcdhe:3A1YMds3H0I0cF-BYDk3tzOyxH@ec2-54-163-230-104.compute-1.amazonaws.com:5432/d1jn05vfkaonph/ssl=true",
+    connection: process.env.DATABASE_URL + '/ssl=true',
     pool: {
       min: 2,
       max: 10
