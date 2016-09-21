a0ad94a5387b8073246b09ba8ddbff541f2ca73f
Zach Buchenau <ZBuchenau@yahoo.com>
2016-09-21 17:56:36 -0600

    remove views stuff

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ app.use(expressJwt({
   }));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
